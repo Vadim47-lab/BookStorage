a55cb769f9a8a8321725fcef8132419081c5caa6
Stepanov Vadim <vadgat777@mail.ru>
2022-07-09 01:59:42 +0700

    first commit

---
+++ BookStorage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
